b1d4325abd27f50421309451002295f0b4bbb950
PrashantGupta8281 <guptaprashant2006@gmail.com>
2026-07-13 07:01:34 +0530

    Revise sidebar content with personal details
    Updated sidebar to include portfolio and personal information.

--- Project6/iris_flower_app.py
+++ Project6/iris_flower_app.py
@@ -266,6 +266,14 @@ st.markdown(
 unsafe_allow_html=True
 )
 #connect with me
-st.sidebar.title("🧑🏻‍💻 Connect with me")
-st.sidebar.markdown("[linkedin](https://www.linkedin.com/in/prashant-gupta-012320389?utm_source=share_via&utm_content=profile&utm_medium=member_android)")
-st.sidebar.markdown("[GitHub](https://github.com/PrashantGupta8281)")
+st.sidebar.title("My Portfolio")
+
+st.sidebar.markdown("""
+### 👋 About Me
+
+**Name:** Prashant Gupta
+
+🔗 **LinkedIn:** [Click Here](https://www.linkedin.com/in/prashant-gupta-012320389?utm_source=share_via&utm_content=profile&utm_medium=member_android)
+
+💻 **GitHub:** [Click Here](https://github.com/PrashantGupta8281/AI-ML_Summer_Internship)
+""")
